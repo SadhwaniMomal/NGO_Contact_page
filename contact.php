a1417771f8ecf28a4d0fd14cbdb6e35552231378
SadhwaniMoomal <sadhwanimoomal987@gmail.com>
2024-11-06 13:17:14 +0500

    inital floder

---
+++ contact.php
@@ -0,0 +1,207 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <!-- Font Awsome Heading -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    <!-- Custom css -->
+     <link rel="stylesheet" href="css/contact.css">
+
+  </head>
+  <body>
+    <!-- main head banner -->
+    <div class="container-fluid common-banner mb-4 ">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="contact-banner-heading ">
+                    <h2>
+                        <span class="big-heading">Contact Us</span>
+                    </h2>
+                    <ul class="breadcrumb_wrapper list-unstyled">
+                        <li class="breadcrumb_item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb_item"><a href="#"><i class="fa-solid fa-arrow-right"></i></a></li>
+                        <li class="breadcrumb_item active">Contact</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Form here  -->
+    <div class="container mb-4">
+            <!-- Heading row -->
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-6 offset-lg-3">
+                    <div class="form-section-heading my-3">
+                        <h3 class="m-0 text-muted">
+                            Contact With Us
+                        </h3>
+                        <h2>Get in
+                                <span class="update_big_heading">touch</span>with us &amp;
+                                stay updates
+                        </h2>
+                    </div>
+                </div>
+            </div>
+            <!-- form row -->
+            <div class="row">
+                <!-- left side -->
+                    <div class="col-md-6">
+                        <div class="contact-left">
+                            <!-- address -->
+                            <div class="contact_left_items">
+                                <div class="contact_left_icon">
+                                    <img src="./assest/map_color.png" alt="icon">
+                                </div>
+                                <div class="contact_left_text">
+                                    <h3>Address:</h3>
+                                    <p>Qasimbad phase 1</p>
+                                </div>
+                            </div>
+
+                            <!-- Email -->
+                            <div class="contact_left_items">
+                                <div class="contact_left_icon">
+                                    <img src="./assest/email-color.png" alt="icon">
+                                </div>
+                                <div class="contact_left_text">
+                                    <h3>Email:</h3>
+                                    <p>support@domain.com</p>
+                                    <p>contact@domain.com</p>
+                                </div>
+                            </div>
+                            <!-- phone -->
+                            <div class="contact_left_items">
+                                <div class="contact_left_icon">
+                                    <img src="./assest/phon-color.png" alt="icon">
+                                </div>
+                                <div class="contact_left_text">
+                                    <h3>Phone Number:</h3>
+                                    <p>+92 311 7322007</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                <!-- right sides -->
+                <div class="col-md-6">
+                    <div class="contact-form-area">
+                        <h3>Leave us a message</h3>
+                        <form action="#!" id="contact_form_data">
+                            <div class="form-group contact-form-group">
+                                <input type="text" class="form-control contact-form-control" placeholder="Your full name*" required="" data-usechatgpt-start-offset="0" data-usechatgpt-end-offset="0" data-usechatgpt-marker="usechatgpt-marker" data-usechatgpt-marker-start-id="usechatgpt-start-marker-707b94af-cd6f-48a6-b6b1-1529984b7d4f" data-usechatgpt-marker-end-id="usechatgpt-end-marker-58c6499a-68ba-4774-80f2-118a472bba35">
+                            </div>
+                            <div class="form-group contact-form-group">
+                                <input type="email" class="form-control contact-form-control" placeholder="Your email address*" required="">
+                            </div>
+                            <div class="form-group contact-form-group">
+                                <input type="text" class="form-control contact-form-control" placeholder="Subject**" required="">
+                            </div>
+                            <div class="form-group contact-form-group">
+                                <textarea class="form-control contact-form-control" rows="6" placeholder="Message*" required=""></textarea>
+                            </div>
+                            <div class="contact_submit_form mb-2">
+                                <button class="btn mb-3">Send message</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    <!-- Map here -->
+    <div class="container-fluid my-5 ">
+        <div class="boxed-content">
+            <div class="container">
+                <div class="row">
+                    <!-- chat live -->
+                    <div class="col-lg-4 col-md-6 col-12 my-5">
+                        <div class="top-boxed bg-chat-area">
+                            <div class="top-boxed-icons">
+                                <img src="./assest/msg.png" alt="">
+                            </div>
+                            <div class="top-boxed-text">
+                                <p>chat with live</p>
+                                <h3>
+                                    chat with our <br>  live experts
+                                </h3>
+                                <a href="#" class="text-decoration-none text-danger fs-5">Chat now</a>
+                            </div>
+                            <div class="top-boxed-vectors">
+                                <img src="./assest/round.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <!-- partner -->
+                    <div class="col-lg-4 col-md-6 col-12 my-5">
+                        <div class="top-boxed bg-partner-area">
+                            <div class="top-boxed-icons">
+                                <img src="./assest/hand-round.png" alt="">
+                            </div>
+                            <div class="top-boxed-text">
+                                <p>Become a partner</p>
+                                <h3>
+                                Enhance your helping <br>  hand for peoples
+                                </h3>
+                                <a href="#" class="text-decoration-none text-danger fs-5">Join now</a>
+                            </div>
+                            <div class="top-boxed-vectors">
+                                <img src="./assest/round.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Ask your questin -->
+                    <div class="col-lg-4 col-md-6 col-12 my-5">
+                        <div class="top-boxed bg-ask-area">
+                            <div class="top-boxed-icons">
+                                <img src="./assest/qa.png" alt="">
+                            </div>
+                            <div class="top-boxed-text">
+                                <p>Ask your questin</p>
+                                <h3>
+                                Ask your any question <br>  and get updates
+                                </h3>
+                                <a href="#" class="text-decoration-none text-danger fs-5">Ask now</a>
+                            </div>
+                            <div class="top-boxed-vectors">
+                                <img src="./assest/round.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="map_area">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3604.3946245624784!2d68.33228467360433!3d25.391597923802642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x394c71bdbb54ac49%3A0x97b86a86651968f2!2sCodePro!5e0!3m2!1sen!2s!4v1730877516522!5m2!1sen!2s" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
